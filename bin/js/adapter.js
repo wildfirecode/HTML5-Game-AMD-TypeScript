aeea2f70de9783ba6184a59eaf19f6181c456a3a
王鸿源honingwon <honingwon@gmail.com>
2017-08-17 10:50:19 +0800

    update

--- bin/js/adapter.js
+++ bin/js/adapter.js
@@ -1,8 +1,5 @@
 define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.EventDispatcher = Laya.EventDispatcher;
-    exports.Handler = Laya.Handler;
-    exports.Animation = Laya.Animation;
     exports.Text = Laya.Text;
 });
--- src/adapter.ts
+++ src/adapter.ts
@@ -1,4 +1,1 @@
-export const EventDispatcher = Laya.EventDispatcher;
-export const Handler = Laya.Handler;
-export const Animation = Laya.Animation;
 export const Text = Laya.Text;
